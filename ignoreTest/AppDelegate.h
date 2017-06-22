f36d29a71bf77a2d6f1c8a975563e2c2a5d0ff98
caofei <1594717129@qq.com>
2017-06-22 14:35:21 +0800

    ignoreTest初始化

---
+++ ignoreTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ignoreTest
+//
+//  Created by 曹飞 on 2017/6/22.
+//  Copyright © 2017年 曹飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ignoreTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ignoreTest
+//
+//  Created by 曹飞 on 2017/6/22.
+//  Copyright © 2017年 曹飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
